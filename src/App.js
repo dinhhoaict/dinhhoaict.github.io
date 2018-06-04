35957345e117a8cfa148751489b947faf8360a9f
hoai ba dinh <dinhhoai1991@gmail.com>
2018-06-04 14:32:38 +0700

    test bootstrap with jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Link, Route } from "react-router-dom";
-import PropTypes from "prop-types";
+import { Link, Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/">Home</Link>
-        </nav>
-        <div>
-          <Route path="/about" component={About} />
-          <Route exact path="/" component={Home} />
-        </div>
-      </div>
-    );
-  }
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<nav>
+					<Link to="/about">About</Link>
+					<Link to="/">Home</Link>
+				</nav>
+				<div>
+					<Route path="/about" component={About} />
+					<Route exact path="/" component={Home} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
+import React from "react";
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from "react-router-dom";
 
