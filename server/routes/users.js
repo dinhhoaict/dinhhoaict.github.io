8eaacbb3263339c6d1163c15a45be2f01fa1e28f
hoai ba dinh <dinhhoai1991@gmail.com>
2018-06-11 17:38:41 +0700

    add some packet

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({"header": 1, "msg": "test"});
+});
+
+router.get('/username/:username/age/:age', function(req, res, next) {
+  var username = req.params["username"];
+  var request = {"params": req.params , "query": req.query, "param": req.param};
+  res.json(request);
 });
 
 module.exports = router;
